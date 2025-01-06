7075e1c9bd35fd354e45a52fdf7249c0cb0a63f8
aashil2495 <120294200+aashil2495@users.noreply.github.com>
2025-01-05 22:53:28 -0600

    Update Azure and Databricks CLI.bat

--- scripts/Azure and Databricks CLI.bat
+++ scripts/Azure and Databricks CLI.bat
@@ -16,6 +16,9 @@ set BRONZE_STORAGE_ACCOUNT_CONTAINER=container-bronze
 set SILVER_STORAGE_ACCOUNT_CONTAINER=container-silver
 set GOLD_STORAGE_ACCOUNT_CONTAINER=container-gold
 set RAW_LAYER_EXTERNAL_LOC_NAME=raw-ext-loc
+set BRONZE_LAYER_EXTERNAL_LOC_NAME=bronze-ext-loc
+set SILVER_LAYER_EXTERNAL_LOC_NAME=silver-ext-loc
+set GOLD_LAYER_EXTERNAL_LOC_NAME=gold-ext-loc
 set ACCESS_CONNECTOR_NAME=ashil-access-connector
 set METASTORE_NAME=southcentralus
 
