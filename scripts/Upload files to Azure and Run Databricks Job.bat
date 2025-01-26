37d853f5c1e5cef12922fe9a73e15ddec8e37617
aashil2495 <120294200+aashil2495@users.noreply.github.com>
2025-01-26 10:24:01 -0600

    Removed "ashil" from parameters

--- scripts/Upload files to Azure and Run Databricks Job.bat
+++ scripts/Upload files to Azure and Run Databricks Job.bat
@@ -1,12 +1,12 @@
 @echo off
 :: Set Variables
 ::Enter the full path to file
-set LOCAL_PATH_TO_FILE=C:\Users\aashi\projects\Databricks CLI\data\green_tripdata_2024-02.parquet
-set DESTINATION_STORAGE_ACCOUNT_NAME=ashilstorage
+set LOCAL_PATH_TO_FILE=yourpath\green_tripdata_2024-02.parquet
+set DESTINATION_STORAGE_ACCOUNT_NAME=itmstorage
 set DESTINATION_STORAGE_CONTAINER_NAME=container-raw
 set DESTINATION_FILE_NAME=green_tripdata_2024-02.parquet
 ::Enter the full path to file
-set LOCAL_JOB_CONFIG_PATH=C:\Users\aashi\projects\Databricks CLI\config\Databricks_Job_Config.json
+set LOCAL_JOB_CONFIG_PATH=yourpath\Databricks_Job_Config.json
 
 
 :: Login with azcopy
